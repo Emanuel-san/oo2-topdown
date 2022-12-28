d442d852037a3256c2618439d698fc9b12fb5e82
Emanuel-san <emanuel.sanchez@ha.ax>
2022-12-28 21:38:10 +0200

    inital project setup and commit

---
+++ core/src/com/mygdx/game/TopDown.java
@@ -0,0 +1,16 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class TopDown extends ApplicationAdapter {
+
+	
+	@Override
+	public void create () {
+
+	}
+
+}
