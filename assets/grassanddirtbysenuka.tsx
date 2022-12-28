774e320f45ffc485b37ee3ab830c52535034b1b3
Emanuel-san <emanuel.sanchez@ha.ax>
2022-12-28 22:03:05 +0200

    added assets and made a tiled test map

---
+++ assets/grassanddirtbysenuka.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="grassanddirtbysenuka" tilewidth="16" tileheight="16" tilecount="30" columns="5">
+ <image source="grassanddirtbysenuka.png" width="80" height="96"/>
+</tileset>
