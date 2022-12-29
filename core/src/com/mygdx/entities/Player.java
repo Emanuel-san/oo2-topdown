bbc1575767248448f3b10b59a62a0c5dabcafb7f
Emanuel-san <emanuel.sanchez@ha.ax>
2022-12-29 23:22:21 +0200

    added delay to player shooting

--- core/src/com/mygdx/entities/Player.java
+++ core/src/com/mygdx/entities/Player.java
@@ -5,25 +5,32 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.utils.Timer;
+import com.badlogic.gdx.utils.Timer.*;
 import com.mygdx.game.GameScreen;
 import com.mygdx.helper.BodyHelper;
 import com.mygdx.helper.ProjectileManager;
-
-import java.util.List;
 
 import static com.mygdx.helper.Constant.PPM;
 
 public class Player extends GameEntity{
     private final Texture character;
     private ProjectileManager projectileManager;
+    private boolean keyDown;
+    private Timer timer = new Timer();
+    private Task task = new Task() {
+        @Override
+        public void run() {
+            keyDown = false;
+        }
+    };
+
 
     public Player(float width, float height, Body body){
         super(width, height, body);
         this.speed = 4f*PPM;
         character = new Texture(Gdx.files.internal("topdown_shooter/characters/1.png"));
-        //System.out.println(body.getPosition().x);
-        //System.out.println(body.getPosition().y);
-
+        keyDown = false;
     }
 
     @Override
@@ -55,8 +62,10 @@ public class Player extends GameEntity{
         }
         body.setLinearVelocity(velX*speed, velY*speed);
 
-        if(Gdx.input.isTouched()){
+        if(Gdx.input.isTouched() && !keyDown){
             projectileManager.createProjectile();
+            keyDown = true;
+            timer.scheduleTask(task, 0.5f);
         }
     }
 
